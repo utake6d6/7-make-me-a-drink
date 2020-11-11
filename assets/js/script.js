64e4934af9643166258b68af8eb0cfb346640ccb
pat paggi <patpaggi@gmail.com>
2020-11-11 10:35:12 -0700

    refresh button

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,26 +10,26 @@ var glass = document.createElement('img');
 var glassEl = document.querySelector('#glass');
 var lists = document.querySelector('#list');
 // var drinkEl = document.querySelector('#drink')
-var liquorEl = document.querySelector('#liquor')
-var grid = document.querySelector('.grid')
+var liquorEl = document.querySelector('#liquor');
+var grid = document.querySelector('.grid');
 var factData = [];
-var factList = document.querySelector('#factList')
-var recent = document.querySelector('#pastSearches')
+var factList = document.querySelector('#factList');
+var recent = document.querySelector('#pastSearches');
 var searchInput = document.querySelector('#drinkSearch');
 var search = JSON.parse(localStorage.getItem('search')) || [];
-
+var refresh = document.querySelector('#refresh');
 
 function getdrink() {
   Promise.all([
-      fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + liquor),
-      fetch("https://google-search3.p.rapidapi.com/api/v1/search/q=" + "fun facts about" + liquor + "=6", {
-        "method": "GET",
-        "headers": {
-          "x-rapidapi-key": "0648fc4c2fmsh626d7d99380e5bap1d3459jsn18d68de57084",
-          "x-rapidapi-host": "google-search3.p.rapidapi.com"
-        }
-      })
-    ])
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + liquor),
+    fetch("https://google-search3.p.rapidapi.com/api/v1/search/q=" + "fun facts about" + liquor + "=6", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "0648fc4c2fmsh626d7d99380e5bap1d3459jsn18d68de57084",
+        "x-rapidapi-host": "google-search3.p.rapidapi.com"
+      }
+    })
+  ])
     .then(function (responses) {
       return Promise.all(responses.map(function (response) {
         return response.json();
@@ -66,6 +66,7 @@ var recipeUl = document.querySelector('#recipe')
 // THis is the recipe!! done until getDrinks is up
 function showRecipe() {
   grid.style.display = "none"
+
   var ingredients = [recipe.drinks[0].strIngredient1, recipe.drinks[0].strIngredient2, recipe.drinks[0].strIngredient3, recipe.drinks[0].strIngredient4, recipe.drinks[0].strIngredient5, recipe.drinks[0].strIngredient6, recipe.drinks[0].strIngredient7, recipe.drinks[0].strIngredient8, recipe.drinks[0].strIngredient9, recipe.drinks[0].strIngredient10, recipe.drinks[0].strIngredient11, recipe.drinks[0].strIngredient12, recipe.drinks[0].strIngredient13, recipe.drinks[0].strIngredient14, recipe.drinks[0].strIngredient15];
   var measure = [recipe.drinks[0].strMeasure1, recipe.drinks[0].strMeasure2, recipe.drinks[0].strMeasure3, recipe.drinks[0].strMeasure4, recipe.drinks[0].strMeasure5, recipe.drinks[0].strMeasure6, recipe.drinks[0].strMeasure7, recipe.drinks[0].strMeasure8, recipe.drinks[0].strMeasure9, recipe.drinks[0].strMeasure10, recipe.drinks[0].strMeasure11, recipe.drinks[0].strMeasure12, recipe.drinks[0].strMeasure13, recipe.drinks[0].strMeasure14, recipe.drinks[0].strMeasure15];
 
@@ -132,6 +133,11 @@ function findDrink(drinkEl) {
 var pickDrink = document.querySelector('#pick-drink')
 // Click on img to show the list of drinks
 function img(pickDrink) {
+  refresh.innerHTML = ""
+  var refreshBtn = document.createElement("button");
+  refreshBtn.setAttribute('Onclick', 'window.location.reload ()')
+  refreshBtn.innerText = ('Refresh')
+  refresh.appendChild(refreshBtn)
   grid.style.display = "none"
   liquor = pickDrink;
   liquorEl.value = liquor;
@@ -141,12 +147,12 @@ function img(pickDrink) {
 
 function getFacts() {
   fetch("https://google-search3.p.rapidapi.com/api/v1/search/q=" + "fun facts about" + liquor + "=11", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-key": "0648fc4c2fmsh626d7d99380e5bap1d3459jsn18d68de57084",
-        "x-rapidapi-host": "google-search3.p.rapidapi.com"
-      }
-    })
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "0648fc4c2fmsh626d7d99380e5bap1d3459jsn18d68de57084",
+      "x-rapidapi-host": "google-search3.p.rapidapi.com"
+    }
+  })
     .then((response) => {
       return response.json();
     })
